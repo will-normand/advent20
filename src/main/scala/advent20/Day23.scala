29acd84171846b614dc1b10db18279146b6ea4f4
Will Normand <mail@wnormand.co.uk>
2020-12-28 23:50:42 +0000

    Day 23 (part 2)
    In a reasonable time.  A bit overcomplicated.

--- src/main/scala/advent20/Day23.scala
+++ src/main/scala/advent20/Day23.scala
@@ -4,14 +4,14 @@ import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
 object Day23 extends App {
-  val input = "389125467"
+  val input = "315679824"
   val cups = ListBuffer.empty ++ input.map(_.toString.toInt)
 
   println(cups)
-  val answer1 = playPart1(cups)
+  val answer1 = playPart1(cups.toList)
   println("Answer (part 1) " + answer1)
 
-  val answer2 = playPart2(cups)
+  val answer2 = playPart2(cups.toList)
   println("Answer (part 2) " + answer2)
 
   def play(cups: ListBuffer[Int], rounds: Int): ListBuffer[Int] = {
@@ -19,9 +19,6 @@ object Day23 extends App {
     val lowestLabel = cups.min
     val highestLabel = cups.max
     for (round <- 1 to rounds) {
-      if ((round % 100000) == 0) {
-        println("round " + round)
-      }
       val currentLabel = cups(index)
       val removed = removeThree(cups, index)
       val destination = destinationCupIndex(cups, currentLabel - 1)
@@ -53,7 +50,7 @@ object Day23 extends App {
     cups
   }
 
-  def playPart1(cups: ListBuffer[Int]): String = {
+  def playPart1Lists(cups: ListBuffer[Int]): String = {
     def result(cups: ListBuffer[Int]): String = {
       val indexOfOne = cups.indexOf(1)
       val result = cups.drop(indexOfOne + 1) ++ cups.take(indexOfOne)
@@ -64,26 +61,110 @@ object Day23 extends App {
     result(resultCups)
   }
 
-  def playPart2(cups: ListBuffer[Int]): BigInt = {
-    def result(cups: ListBuffer[Int]): BigInt = {
-      val indexOfOne = cups.indexOf(1)
+  def playPart1(cupLabels: List[Int]): String = {
+    def result(cups: Cups): String = cups.labelsFromOne
 
-      val results = cups.slice(indexOfOne + 1, indexOfOne + 3)
-      println(results)
-      println(indexOfOne)
-      val cup1 = BigInt(results(0))
-      val cup2 = BigInt(results(1))
-      val result: BigInt = cup1 * cup2
-      println(s"cup1: $cup1, cup2: $cup2, result: $result")
+    val cupRing = Cup.createCups(cupLabels)
+    val cups = Cups(cupRing)
+    for (_ <- 1 to 100) { cups.move() }
+    result(cups)
+  }
+
+  def playPart2(cupLabels: List[Int]): BigInt = {
+    def result(cups: Cups): BigInt = {
+      val oneCup = cups.lookup(1)
+
+      val nextCup = BigInt(oneCup.next.get.label)
+      val nextNextCup = BigInt(oneCup.next.get.next.get.label)
+      val result: BigInt = nextCup * nextNextCup
       result
     }
 
-    val extraCups = (cups.max + 1) to 1000000
-    val allCups = cups ++ extraCups
-    println("All cups size " + allCups.size)
-    println(allCups take 20)
+    val cupRing = Cup.createCupsPadded(cupLabels, 1000000)
+    val cups = Cups(cupRing)
+    for (_ <- 1 to 10000000) { cups.move() }
+    result(cups)
+  }
+}
 
-    val resultCups = play(allCups, 10000000)
-    result(resultCups)
+class Cups(var current: Cup, lookupTable: Map[Int, Cup], minLabel: Int, maxLabel: Int) {
+  def lookup(label: Int): Cup = lookupTable(label)
+
+  def move(): Unit = {
+    val movingFirst = current.next.get
+    val movingLast = current.next3
+    current.next = movingLast.next
+    movingLast.next = None
+    val movingLabels = Set(current.label, movingFirst.label, movingFirst.next.get.label, movingLast.label)
+    var nextLabel = decrementLabel(current.label)
+    while (movingLabels.contains(nextLabel)) {
+      nextLabel = decrementLabel(nextLabel)
+    }
+    val nextCup = lookup(nextLabel)
+    val afterInsert = nextCup.next.get
+    nextCup.next = Some(movingFirst)
+    movingLast.next = Some(afterInsert)
+    current = current.next.get
+  }
+
+  def labelsFromOne: String = {
+    val oneCup = lookup(1)
+    var cup = oneCup.next.get
+
+    val labels: ListBuffer[Int] = ListBuffer.empty
+    while (cup != oneCup) {
+      labels append cup.label
+      cup = cup.next.get
+    }
+    labels.map(_.toString).mkString
+  }
+
+  private def decrementLabel(label: Int): Int = if (label - 1 < minLabel) maxLabel else label - 1
+}
+
+object Cups {
+  def apply(current: Cup): Cups = {
+    var lookup: Map[Int, Cup] = Map(current.label -> current)
+    var cup = current.next.get
+    var minLabel = Int.MaxValue
+    var maxLabel = 0
+    while (cup != current) {
+      lookup = lookup + (cup.label -> cup)
+      if (cup.label < minLabel) minLabel = cup.label
+      if (cup.label > maxLabel) maxLabel = cup.label
+      cup = cup.next.get
+    }
+
+    new Cups(current, lookup, minLabel, maxLabel)
+  }
+}
+
+case class Cup(label: Int, var next: Option[Cup]) {
+  def next3: Cup = next.get.next.get.next.get
+  override def toString: String = s"Cup($label, next=${next.get.label})"
+}
+
+case object Cup {
+  def createCups(cups: List[Int]): Cup = {
+    @tailrec
+    def createCups0(cups: List[Int], prevCup: Cup, firstCup: Cup): Cup = {
+      cups match {
+        case x :: xs => createCups0(xs, new Cup(x, Some(prevCup)), firstCup)
+        case Nil =>
+          firstCup.next = Some(prevCup)
+          prevCup
+      }
+    }
+
+    val cupsReversed = cups.reverse
+    val firstCup = new Cup(cupsReversed.head, Option.empty)
+    createCups0(cupsReversed.tail, firstCup, firstCup)
+  }
+
+  def createCupsPadded(cups: List[Int], padding: Int): Cup = {
+    val extraCups = (cups.max + 1) to padding
+    val allCups = cups ++ extraCups
+    assert(allCups.size == padding)
+    createCups(allCups)
   }
 }
---
+++ src/test/scala/advent20/Day23Test.scala
@@ -0,0 +1,90 @@
+package advent20
+
+import advent20.Day23.{playPart1, playPart1Lists, playPart2}
+import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.collection.mutable.ListBuffer
+
+class Day23Test extends AnyFlatSpec {
+
+  behavior of "Part 1 original"
+
+  it should "work for the example" in {
+    assertResult("67384529") {
+      val cups = ListBuffer.empty ++ "389125467".map(_.toString.toInt)
+      playPart1Lists(cups)
+    }
+  }
+
+  behavior of "Part 1 faster version"
+
+  it should "work for the example" in {
+    assertResult("67384529") {
+      val cups = ListBuffer.empty ++ "389125467".map(_.toString.toInt)
+      playPart1(cups.toList)
+    }
+  }
+
+
+  behavior of "Part 2"
+
+  it should "work for the example" in {
+    assertResult(BigInt("149245887792")) {
+      val cups = "389125467".map(_.toString.toInt).toList
+      playPart2(cups)
+    }
+  }
+
+  behavior of "Cup"
+
+  it should "create a loop" in {
+    assertResult(2) {
+      val cups = Cup.createCups(List(1, 2, 3))
+      cups.next.get.next.get.next.get.next.get.label
+    }
+  }
+
+  it should "work for a big list" in {
+    assertResult(5) {
+      val cups = Cup.createCupsPadded(List(1, 2, 3), 100000)
+      cups.next.get.next.get.next.get.next.get.label
+    }
+  }
+
+  behavior of "Cups"
+
+  it should "allow looking up cups" in {
+    assertResult(12345 ) {
+      val cupRing = Cup.createCupsPadded(List(1, 2, 3), 100000)
+      val cups = Cups(cupRing)
+      cups.lookup(12344).next.get.label
+    }
+  }
+
+  it should "do a move" in {
+    val cups = Cups(Cup.createCups(List(1, 2, 3, 4, 5, 6)))
+    cups.move()
+
+    assertResult(5)(cups.current.label)
+    assertResult(6)(cups.current.next.get.label)
+    assertResult(2)(cups.current.next.get.next.get.label)
+    assertResult(3)(cups.current.next.get.next.get.next.get.label)
+    assertResult(4)(cups.current.next.get.next.get.next.get.next.get.label)
+    assertResult(1)(cups.current.next.get.next.get.next.get.next.get.next.get.label)
+    assertResult(5)(cups.current.next.get.next.get.next.get.next.get.next.get.next.get.label)
+  }
+
+  it should "do a second move" in {
+    val cups = Cups(Cup.createCups(List(1, 2, 3, 4, 5, 6)))
+    cups.move()
+    cups.move()
+
+    assertResult(4)(cups.current.label)
+    assertResult(6)(cups.current.next.get.label)
+    assertResult(2)(cups.current.next.get.next.get.label)
+    assertResult(3)(cups.current.next.get.next.get.next.get.label)
+    assertResult(1)(cups.current.next.get.next.get.next.get.next.get.label)
+    assertResult(5)(cups.current.next.get.next.get.next.get.next.get.next.get.label)
+    assertResult(4)(cups.current.next.get.next.get.next.get.next.get.next.get.next.get.label)
+  }
+}
