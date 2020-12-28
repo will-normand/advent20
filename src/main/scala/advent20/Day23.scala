fcd189da254ae0ad2b23a5d0baf2dc5dcfb9243a
Will Normand <mail@wnormand.co.uk>
2020-12-28 20:36:29 +0000

    Day 23 (part 2) - slow

--- src/main/scala/advent20/Day23.scala
+++ src/main/scala/advent20/Day23.scala
@@ -1,22 +1,27 @@
 package advent20
 
 import scala.annotation.tailrec
-import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object Day23 extends App {
-  val input = "315679824"
+  val input = "389125467"
   val cups = ListBuffer.empty ++ input.map(_.toString.toInt)
 
   println(cups)
-  val answer1 = play(cups)
+  val answer1 = playPart1(cups)
   println("Answer (part 1) " + answer1)
 
-  def play(cups: ListBuffer[Int]): String = {
+  val answer2 = playPart2(cups)
+  println("Answer (part 2) " + answer2)
+
+  def play(cups: ListBuffer[Int], rounds: Int): ListBuffer[Int] = {
     var index = 0
     val lowestLabel = cups.min
     val highestLabel = cups.max
-    for (_ <- 1 to 100) {
+    for (round <- 1 to rounds) {
+      if ((round % 100000) == 0) {
+        println("round " + round)
+      }
       val currentLabel = cups(index)
       val removed = removeThree(cups, index)
       val destination = destinationCupIndex(cups, currentLabel - 1)
@@ -45,12 +50,40 @@ object Day23 extends App {
       removed
     }
 
+    cups
+  }
+
+  def playPart1(cups: ListBuffer[Int]): String = {
     def result(cups: ListBuffer[Int]): String = {
       val indexOfOne = cups.indexOf(1)
       val result = cups.drop(indexOfOne + 1) ++ cups.take(indexOfOne)
       result.map(_.toString).mkString
     }
 
-    result(cups)
+    val resultCups = play(cups, 100)
+    result(resultCups)
+  }
+
+  def playPart2(cups: ListBuffer[Int]): BigInt = {
+    def result(cups: ListBuffer[Int]): BigInt = {
+      val indexOfOne = cups.indexOf(1)
+
+      val results = cups.slice(indexOfOne + 1, indexOfOne + 3)
+      println(results)
+      println(indexOfOne)
+      val cup1 = BigInt(results(0))
+      val cup2 = BigInt(results(1))
+      val result: BigInt = cup1 * cup2
+      println(s"cup1: $cup1, cup2: $cup2, result: $result")
+      result
+    }
+
+    val extraCups = (cups.max + 1) to 1000000
+    val allCups = cups ++ extraCups
+    println("All cups size " + allCups.size)
+    println(allCups take 20)
+
+    val resultCups = play(allCups, 10000000)
+    result(resultCups)
   }
 }
